3dee8a8a3e10735a18fa4eabeab20c7b9279e3e0
Cara Bunnell <carabunnell@gmail.com>
2020-05-15 17:15:16 -0700

    pushing full code

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    console.log(registration);
     console.log('ServiceWorker registration successful with scope:',  registration.scope);
   }).catch(function(error) {
     console.log('ServiceWorker registration failed:', error);
